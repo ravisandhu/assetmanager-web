8162d9ed956d47e63f82ddcf65206169f006f93e
Simon Emms <simon@simonemms.com>
2014-10-26 21:22:40 +0000

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assetmanagerWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the assetmanagerWebApp
+ */
+angular.module('assetmanagerWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assetmanagerWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the assetmanagerWebApp
+ */
+angular.module('assetmanagerWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
