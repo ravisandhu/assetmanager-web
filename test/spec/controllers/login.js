21ce0c46e07200d2971e86fd8e19d114af186236
Simon Emms <simon@simonemms.com>
2014-10-27 20:40:08 +0000

    Added some tests around the login controller

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -16,4 +16,50 @@ describe('Controller: LoginCtrl', function () {
     });
   }));
 
+  describe('Static data', function () {
+
+    it('should define the formData', function () {
+
+      expect(scope.formData).to.be.eql({});
+
+    });
+
+    it('should define the formFields', function () {
+
+      expect(scope.formFields).to.be.eql([{
+        key: 'emailAddress',
+        type: 'email',
+        label: 'Email address'
+      }, {
+        key: 'password',
+        type: 'password',
+        label: 'Password'
+      }]);
+
+    });
+
+    it('should define the formOptions', function () {
+
+      expect(scope.formOptions).to.be.eql({
+        uniqueFormId: 'login'
+      });
+
+    });
+
+  });
+
+  describe('Methods', function () {
+
+    describe('login', function () {
+
+      it('should login to the API');
+
+      it('should fail to login to the API');
+
+      it('should handle an API error');
+
+    });
+
+  });
+
 });
