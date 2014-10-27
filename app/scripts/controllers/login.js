8d65f54479f6b3a1af5df7d4a0478d66566616fb
Simon Emms <simon@simonemms.com>
2014-10-27 18:33:57 +0000

    Added login route with angular-formly

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assetmanagerWebApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the assetmanagerWebApp
+ */
+angular.module('assetmanagerWebApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
