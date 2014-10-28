a9d9bf6cd82871d3bf715f8a05f429da2a9f8bc3
Simon Emms <simon@simonemms.com>
2014-10-28 21:30:14 +0000

    Added the API factory

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -8,7 +8,7 @@
  * Factory in the assetmanagerWebApp.
  */
 angular.module('assetmanagerWebApp')
-  .factory('api', function ($http) {
+  .factory('api', function ($http, $q, apiUrl, apiauth) {
 
 
     /**
@@ -23,19 +23,56 @@ angular.module('assetmanagerWebApp')
      */
     var apiCall = function (method, url, params, data) {
 
-      console.log(arguments);
+      return $http({
+        method: method.toUpperCase(),
+        url: apiUrl + url,
+        params: params,
+        data: data,
+        headers: {
+          accept: 'application/json'
+        },
+        timeout: 30000
+      });
 
     };
 
 
     return {
 
+      /**
+       * Login
+       *
+       * Logs into the API
+       *
+       * @param {string} emailAddress
+       * @param {string} password
+       */
       login: function (emailAddress, password) {
+
+        var defer = $q.defer();
 
         apiCall('post', '/system/web', null, {
           emailAddress: emailAddress,
           password: password
+        }).then(function (obj) {
+
+          apiauth.saveAuth(obj.data.sessionId);
+
+          defer.resolve(true);
+
+        }).catch(function (obj) {
+
+          /* Failed - is auth or server error? */
+          if (obj.status === 401) {
+            /* No error - just not valid credentials */
+            defer.resolve(false);
+          } else {
+            defer.reject();
+          }
+
         });
+
+        return defer.promise;
 
       }
 
